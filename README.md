758c08cf3c50665a7cd89cac89ec2e3ebf06ffbf
Michael Stewart <stewarmc@jmu.edu>
2026-02-24 21:48:34 -0500

    before settings update

---
+++ README.md
@@ -0,0 +1,14 @@
+
+## How I done did this tho?
+
+1. git init
+2. created
+    1. .gitignore
+    2. .venv
+    3. .env.local
+    4. requirements.txt
+3. activated venv
+4. installed pip packages from requirements.txt
+5. `django-admin startproject ticreactoe .`
+6. `python manage.py startapp game`
+7. edit game/models.py
---
+++ ticreactoe/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ticreactoe project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticreactoe.settings')
+
+application = get_wsgi_application()
